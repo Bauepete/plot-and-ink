46f96017f649add98c829f76e507a69e821f1e3f
Peter Bauer <p.bauer@htl-leonding.ac.at>
2023-02-25 13:36:49 +0100

    Removed some displaced files

--- Package.swift
+++ Package.swift
@@ -5,21 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "PlotAndInk",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "PlotAndInk",
+            targets: ["PlotAndInk"]),
+    ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/plot.git", .branch("master")),
-        .package(url: "https://github.com/johnsundell/ink.git", .branch("master")),
-        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.4")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "PlotAndInk",
-            dependencies: [
-                .product(name: "Plot", package: "plot"),
-                .product(name: "Ink", package: "ink"),
-                .product(name: "Files", package: "Files")
-            ]),
+            dependencies: []),
         .testTarget(
             name: "PlotAndInkTests",
             dependencies: ["PlotAndInk"]),
---
+++ Sources/PlotAndInk/PlotAndInk.swift
@@ -0,0 +1,6 @@
+public struct PlotAndInk {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
